825136b4ba90ec5d8bf7987f059fa510eb125667
NARUBROWN <codingshark@inu.ac.kr>
2024-07-25 13:55:16 +0900

    [Feat] WebClient로 교체

--- src/main/java/com/example/blog/controller/OauthController.java
+++ src/main/java/com/example/blog/controller/OauthController.java
@@ -2,69 +2,55 @@ package com.example.blog.controller;
 
 import com.example.blog.data.dto.TokenResDto;
 import com.example.blog.data.dto.UserInfoResDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/oauth")
 @Slf4j
+@RequiredArgsConstructor
 public class OauthController {
     @Value("${client_id}")
     private String clientId;
-
+    private final WebClient webClient;
     @GetMapping("/kakao")
     ResponseEntity<?> kakaoLogin(@RequestParam String code) {
         log.info("kakao login code: " + code);
 
-        RestTemplate rt = new RestTemplate();
+        String requestUri = UriComponentsBuilder.fromHttpUrl("https://kauth.kakao.com/oauth/token")
+                        .queryParam("grant_type", "authorization_code").queryParam("client_id", clientId)
+                        .queryParam("redirect_uri", "http://localhost:8080/oauth/kakao").queryParam("code", code)
+                        .toUriString();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        TokenResDto tokenResDto =
+                webClient.post()
+                .uri(requestUri)
+                .retrieve()
+                .bodyToMono(TokenResDto.class)
+                .block();
 
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", clientId);
-        params.add("redirect_uri", "http://localhost:8080/oauth/kakao");
-        params.add("code", code);
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-        ResponseEntity<TokenResDto> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                request,
-                TokenResDto.class
-        );
-
-        TokenResDto tokenResDto = response.getBody();
 
         return ResponseEntity.status(HttpStatus.OK).body(tokenResDto.getAccess_token());
     }
 
     @GetMapping("/kakaoGetUserInfo")
     ResponseEntity<?> kakaoGetUserInfo(@RequestParam String AccessCode) {
-            RestTemplate rt = new RestTemplate();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Authorization", "Bearer "+ AccessCode);
-            headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        UserInfoResDto response = webClient.post()
+                .uri("https://kapi.kakao.com/v2/user/me")
+                .header("Authorization", "Bearer " + AccessCode)
+                .retrieve()
+                .bodyToMono(UserInfoResDto.class)
+                .block();
 
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(null, headers);
-        ResponseEntity<UserInfoResDto> response = rt.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                request,
-                UserInfoResDto.class
-        );
-        // -> 인가 -> Access Token 받고 -> 사용자가 가입자 or 가입자 아닌지 판별 -> 만약에 가입자가 아니면 -> 프론트에 리다이렉트 시킬 수 있도록
-
-        return ResponseEntity.status(HttpStatus.OK).body(response.getBody());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
