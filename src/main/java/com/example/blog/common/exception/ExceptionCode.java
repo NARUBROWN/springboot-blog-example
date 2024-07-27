03533ee0e4162ffaf6ff71d50b08ed74903efcf8
NARUBROWN <codingshark@inu.ac.kr>
2024-07-27 15:39:07 +0900

    [Refactor] Optional 사용

--- src/main/java/com/example/blog/common/exception/ExceptionCode.java
+++ src/main/java/com/example/blog/common/exception/ExceptionCode.java
@@ -12,7 +12,10 @@ public enum ExceptionCode {
 
     // Security
     SECURITY_RESOURCE_DENIED(HttpStatus.BAD_REQUEST, "리소스에 접근할 권한이 없습니다."),
-    SECURITY_NOT_AUTH_USER(HttpStatus.BAD_REQUEST, "인증되지 않은 사용자입니다.")
+    SECURITY_NOT_AUTH_USER(HttpStatus.BAD_REQUEST, "인증되지 않은 사용자입니다."),
+
+    // WebClient
+    WEB_CLIENT_ERROR(HttpStatus.BAD_REQUEST, "Web Client에서 요청을 가져오는데 실패했습니다.")
     ;
 
     // Post
--- src/main/java/com/example/blog/controller/OauthController.java
+++ src/main/java/com/example/blog/controller/OauthController.java
@@ -1,5 +1,7 @@
 package com.example.blog.controller;
 
+import com.example.blog.common.exception.CustomException;
+import com.example.blog.common.exception.ExceptionCode;
 import com.example.blog.data.dto.TokenResDto;
 import com.example.blog.data.dto.UserInfoResDto;
 import com.example.blog.service.OauthService;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/oauth")
 @Slf4j
@@ -26,27 +30,28 @@ public class OauthController {
 
     @GetMapping("/kakao")
     ResponseEntity<?> kakaoLogin(@RequestParam String code) {
-        log.info("kakao login code: " + code);
 
         String requestUri = UriComponentsBuilder.fromHttpUrl("https://kauth.kakao.com/oauth/token")
                         .queryParam("grant_type", "authorization_code").queryParam("client_id", clientId)
                         .queryParam("redirect_uri", "http://localhost:8080/oauth/kakao").queryParam("code", code)
                         .toUriString();
 
-        TokenResDto tokenResDto =
+        Optional<TokenResDto> tokenResDtoOptional =
                 webClient.post()
                 .uri(requestUri)
                 .retrieve()
                 .bodyToMono(TokenResDto.class)
-                .block();
+                .blockOptional();
 
-        UserInfoResDto response = webClient.post()
+        TokenResDto tokenResDto = tokenResDtoOptional.orElseThrow(() -> new CustomException(ExceptionCode.WEB_CLIENT_ERROR));
+
+        Optional<UserInfoResDto> response = webClient.post()
                 .uri("https://kapi.kakao.com/v2/user/me")
                 .header("Authorization", "Bearer " + tokenResDto.getAccess_token())
                 .retrieve()
                 .bodyToMono(UserInfoResDto.class)
-                .block();
+                .blockOptional();
 
-        return ResponseEntity.status(HttpStatus.OK).body(oauthService.register(response));
+        return ResponseEntity.status(HttpStatus.OK).body(oauthService.register(response.orElseThrow(() -> new CustomException(ExceptionCode.WEB_CLIENT_ERROR))));
     }
 }
